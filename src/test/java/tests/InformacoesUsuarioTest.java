1b181ba65aa1dee34e6f55ef8b622d4ec32df469
Allyson-mkw <allysoneduardo04@gmail.com>
2021-05-24 15:30:51 -0300

    feat: added chromedriver and open chrome

--- src/test/java/tests/InformacoesUsuarioTest.java
+++ src/test/java/tests/InformacoesUsuarioTest.java
@@ -1,5 +1,16 @@
 package tests;
 
-public class InformacoesUsuarioTest {
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class InformacoesUsuarioTest {
+    @Test
+    public void testAdicionarUmaInformacaoAdicionalDoUsuario() {
+        System.setProperty("webdriver.chrome.driver", "C:\\projects\\assets\\drivers\\chromedriver.exe");
+        WebDriver navegador = new ChromeDriver();
+        assertEquals(1, 1);
+        
+    }
 }
