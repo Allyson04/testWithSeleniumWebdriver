9e985672dd5e22fda557c4614f048ebe94c5b3cf
Allyson-mkw <allysoneduardo04@gmail.com>
2021-05-24 21:48:35 -0300

    feat: added test in webpage'taskit.com' of Júlio de Lima

--- src/test/java/tests/InformacoesUsuarioTest.java
+++ src/test/java/tests/InformacoesUsuarioTest.java
@@ -1,8 +1,11 @@
 package tests;
 
+import java.util.concurrent.TimeUnit;
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class InformacoesUsuarioTest {
@@ -13,6 +16,7 @@ public class InformacoesUsuarioTest {
         
         //create a variable to indicate the instruction using chromedriver
         WebDriver navegador = new ChromeDriver();
+        navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         
         //go to webpage
         navegador.get("http://www.juliodelima.com.br/taskit");
@@ -20,8 +24,25 @@ public class InformacoesUsuarioTest {
         //maximize browser window
         navegador.manage().window().maximize();
         
+        //click on "sign in"
+        navegador.findElement(By.linkText("Sign in")).click();
+        
+        //identifying the form
+        WebElement formSigninbox = navegador.findElement(By.id("signinbox"));
+        
+        //type login
+        formSigninbox.findElement(By.name("login")).sendKeys("allyson04");
+
+        //type password
+        formSigninbox.findElement(By.name("password")).sendKeys("123456");
+
+        //click on "sign in" inside the modal "sign in"
+        formSigninbox.findElement(By.linkText("SIGN IN")).click();
+        
         //make a validation
-        assertEquals(1, 1);
+        WebElement confirmMessage = navegador.findElement(By.className("me"));
+        String loginName = confirmMessage.getText();
+        assertEquals("Hi, Allyson Eduardo", loginName);
         
     }
 }
