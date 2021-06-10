f45ce0ea3cd59a3d6524b75df0e17c15a0ec00b5
Allyson-mkw <allysoneduardo04@gmail.com>
2021-06-09 22:57:31 -0300

    feat: added variable parameters for tests in pageObjects

--- src/test/java/tests/UserInfoPageObjects.java
+++ src/test/java/tests/UserInfoPageObjects.java
@@ -1,13 +1,20 @@
 package tests;
 
+import org.easetech.easytest.annotation.DataLoader;
+import org.easetech.easytest.annotation.Param;
+import org.easetech.easytest.runner.DataDrivenTestRunner;
 import org.junit.After;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import pages.LoginPage;
 import support.Web;
 
+
+@RunWith(DataDrivenTestRunner.class)
+@DataLoader(filePaths = "UserInfoTestDataPageObjects.csv")
 public class UserInfoPageObjects {
     private WebDriver navegador;
     
@@ -18,22 +25,29 @@ public class UserInfoPageObjects {
     }
     
     @Test
-    public void testAdicionarUmaInformacaoAdicionalDoUsuario() {
+    public void testAdicionarUmaInformacaoAdicionalDoUsuario(
+            @Param(name="login")String login,
+            @Param(name="password")String password,
+            @Param(name="type")String type,
+            @Param(name="contact")String contact,
+            @Param(name="messageExpected")String messageExpected
+        ) {
+        
         String confirmMessage = new LoginPage(navegador)
                 .clickSignIn()
-                .logIn("allyson04", "123456")
+                .logIn(login,password)
                 .clickHiUser()
                 .openAddMoreDataTab()
                 .clickAddMoreDataButton()
-                .addNewContact("phone", "(21) 965999999")
+                .addNewContact(type, contact)
                 .checkComboBox();
         
-        assertEquals("Your contact has been added!", confirmMessage);
+        assertEquals(messageExpected, confirmMessage);
 
     }
     
     @After
     public void tearDown() {
-//        navegador.quit();
+        navegador.quit();
     }
 }
